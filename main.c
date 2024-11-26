64f5617edff9ff7e878d8c5468b7f61664a41f76
Ivan Gontchar <ivan.tardis@outlook.com>
2024-11-25 23:21:40 -0500

    maybe done with everything? to be checked and worked on later in class/at home

--- main.c
+++ main.c
@@ -11,8 +11,24 @@
 #include <dirent.h>
 #include <sys/wait.h>
 
+int err(){
+    printf("errno %d\n",errno);
+    printf("%s\n",strerror(errno));
+    exit(1);
+}
+
 int main(){
   printf("%d about to create 2 child processes\n", getpid());
+  // wait(WIFEXITED( status ));
+
+  int x,y;
+  int random = open("/dev/random", O_RDONLY, 0300);
+  if(random==-1)err();
+  read(random, &x, sizeof(int));
+  read(random, &y, sizeof(int));
+  x = abs(x)%5+1;
+  y = abs(x)%5+1;
+  // printf("X: %d   Y: %d\n", x, y);
 
   pid_t p1, p2;
   p1 = fork();
@@ -20,18 +36,28 @@ int main(){
     perror("fork fail");//output to stderr instead of stdout
     exit(1);
   } else if ( p1 == 0){
-    int rand;
-    printf("%d %dsec\n", getpid(), rand);
+    printf("%d %dsec\n", getpid(), x);
+    sleep(x);
+    printf("%d finished after %dsec\n", getpid(), x);
+    exit(x);
     // printf("Hello from Child! PID: %d\n", getpid());
   }else{
     p2 = fork();
     if ( p2 == 0){
-      int rand;
-      printf("%d %dsec\n", getpid(), rand);
+      printf("%d %dsec\n", getpid(), y);
+      sleep(y);
+      printf("%d finished after %dsec\n", getpid(), y);
+      exit(y);
       // printf("Hello from Child! PID: %d\n", getpid());
     }
     else{
+      int status;
+      int hold = wait(&status);
+      if( WIFEXITED(status) ){
+        printf("Main Process %d is done. Child %d slept for %dsec\n", getpid(), hold, WEXITSTATUS(status));
+      }
       // printf("Hello from Parent! PID: %d\n", getpid());
     }
   }
+  return 0;
 }
